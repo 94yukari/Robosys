a6184668a2bd465c148a4d5b76786c751c130f7c
Yukari Suzuki <silver-soul.yukari.koara@ezweb.ne.jp>
2017-02-23 07:41:00 +0900

    add a file

---
+++ catkin_ws/build/mypkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yukari/catkin_ws/src/mypkg
+# Build directory: /home/yukari/catkin_ws/build/mypkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
