1bdbf331b94256041bd4d229d6cc7fd5e074703b
Yukari Suzuki <silver-soul.yukari.koara@ezweb.ne.jp>
2017-02-27 14:26:53 +0900

    clean up

--- catkin_ws/build/mypkg2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yukari/catkin_ws/src/mypkg2
-# Build directory: /home/yukari/catkin_ws/build/mypkg2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
